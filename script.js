20e6350f608e1eb6d02c0c32b8cf9666340b10a3
Oscar <exaple@gmail.com>
2022-11-24 10:05:39 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// const btns = document.quarySellectorAll('button');
+//       wrapper = document.querySelector('.btn-block')
+// conlose.log(btns[0].classList.length);
+// console.log(btns[0].classList.item(1));
+// console.log(btns[0].classList.add('red', 'tttrtrtrtt'));
+// // console.log(btns[0].classList.remove('blue'));
+// console.log(btns[0].classList.toggle('blue'));
+
+// if (btns[1].classList.contains('red')) {
+//     console.log('red');
+// }
+
+// btns[0].addEventListener('click', () => {
+//     if (!btns[1].classList.contains('red')) {
+//         btns[1].classList.add('red');
+//     }else {
+//         btns[1].classList.remove('red')
+//     }
+
+//     btns[1].classList.toggle('red');
+// });
+
+// wrapper.addEventListener('click', (event) => {
+//     if (event.targt && event.target.matches("button.red")) {
+//         console.log('Hello');
+//     }
+// });
+
+// btns.forEach(btn => {
+//     console.log('Hello');
+// });
+
+
+// const btn = document.createElement('button');
+// btn.classList.add('red');
+// wrapper.append(btn);
